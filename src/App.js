75730fe982f6b838f3e3353cca92973392407edc
Nikita <norenkonikita@gmail.com>
2021-11-05 01:02:39 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import * as data from './data/left-overs-json.json'
+import ListItem from './ListItem';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: data,
+      term: '',
+      filter: 0,
+      counter: 0
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>I LOVE VIYAR</h1>
+        <ul>
+          <li>
+            <div className="list-item">Код товара</div>
+            <div className="list-item">Номенклатура</div>
+            <div className="list-item">Характеристика</div>
+            <div className="list-item">Толщина</div>
+            <div className="list-item">Количество остаток (м.)</div>
+            <div className="list-item">Количество остаток (шт.)</div>
+            <div className="list-item">Стоимость(шт.)</div>
+          </li>
+          <ListItem listItems={this.state} />
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
