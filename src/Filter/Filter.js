a763b21802bdf4f98c09b61e20b148e411e3e73b
Nikita <norenkonikita@gmail.com>
2021-11-23 16:39:45 +0300

    search width is done

--- src/Filter/Filter.js
+++ src/Filter/Filter.js
@@ -1,4 +1,4 @@
-import { Container, Row} from "react-bootstrap"
+import { Container, Row } from "react-bootstrap"
 import Length from "./Length"
 import Width from "./Width"
 import Height from "./Height"
@@ -13,40 +13,30 @@ class Filter extends Component {
         length: [],
         width: [],
         height: [],
-        widthOnChange: 0,
+        widthOnChange: '',
     }
 
     dataServices = new dataService()
 
     componentDidMount() {
-        this.setState({length: this.dataServices.getLength()})
-        this.setState({width: this.dataServices.getWidth()})
-        this.setState({height: this.dataServices.getHeight()})
+        this.setState({ length: this.dataServices.getLength() })
+        this.setState({ width: this.dataServices.getWidth() })
+        this.setState({ height: this.dataServices.getHeight() })
     }
-    
+
     searchWidth = (items, value) => {
-        if (value === 0) {
+        if (value.length === 0) {
             return items
         }
-        
-        // return items.filter(item => item === value)
-        console.log(items[0].toString()[0]);
-        return items.filter(item => {
-            let a = []
-            if (item.toString[0] === value.toString()[0]) {
-                a.push(item)
-                console.log(a);
-            } else {console.log('else')}
-            return a
-        })
+        return items.filter(item => item.toString().includes(value))
     }
 
     updateWidth = (value) => {
-        this.setState({widthOnChange: +value})
+        this.setState({ widthOnChange: value })
     }
 
     render() {
-        const {length, width, height, widthOnChange} = this.state
+        const { length, width, height, widthOnChange } = this.state
         const visibleWidthValues = this.searchWidth(width, widthOnChange)
         return (
             <Container>
@@ -64,8 +54,8 @@ class Filter extends Component {
     }
 
 
-    
-   
+
+
 }
 
 export default Filter
