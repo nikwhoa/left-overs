266265f500efc1e1ee2bed9566d777b92d3adabe
Nikita <norenkonikita@gmail.com>
2021-11-23 12:29:01 +0300

    change filter.js to class component

--- src/Filter/Filter.js
+++ src/Filter/Filter.js
@@ -6,28 +6,44 @@ import Material from "./Material"
 import Decor from "./Decor"
 
 import dataService from "../service/dataService"
+import { Component } from "react"
 
-const Filter = () => {
+class Filter extends Component {
+    state = {
+        length: [],
+        width: [],
+        height: [],
+    }
 
-    const dataServices = new dataService()
+    dataServices = new dataService()
+
+    componentDidMount() {
+        this.setState({length: this.dataServices.getLength()})
+        this.setState({width: this.dataServices.getWidth()})
+        this.setState({height: this.dataServices.getHeight()})
+    }
+    
+
+    render() {
+        const {length, width, height} = this.state
+        return (
+            <Container>
+                <Row className='justify-content-center'>
+                    <Length class={'text-center'} valuesLength={length} />
+                    <Width valuesWidth={width} />
+                    <Height valuesHeight={height} />
+                </Row>
+                <Row className='justify-content-center'>
+                    <Material class={'justify-content-center'} />
+                    <Decor class={'justify-content-center'} />
+                </Row>
+            </Container>
+        )
+    }
+
 
     
-    const valuesLength = dataServices.getLength()
-    const valuesWidth = dataServices.getWidth()
-    const valuesHeight = dataServices.getHeight()
-    return (
-        <Container>
-            <Row className='justify-content-center'>
-                <Length class={'text-center'} valuesLength={valuesLength} />
-                <Width valuesWidth={valuesWidth} />
-                <Height valuesHeight={valuesHeight} />
-            </Row>
-            <Row className='justify-content-center'>
-                <Material class={'justify-content-center'} />
-                <Decor class={'justify-content-center'} />
-            </Row>
-        </Container>
-    )
+   
 }
 
 export default Filter
