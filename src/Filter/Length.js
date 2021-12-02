c8862de4b1cae9105d00593cc5b17e4c94530d7d
Nikita <norenkonikita@gmail.com>
2021-12-02 23:16:35 +0200

    done with material set up. setuping length component

--- src/Filter/Length.js
+++ src/Filter/Length.js
@@ -1,26 +1,46 @@
+
+import { Component, useState } from "react"
 import { Col, InputGroup, DropdownButton, Dropdown, FormControl } from "react-bootstrap"
 
-const Length = (props) => {
+class Length extends Component {
+    state = {
+        title: 'Длина'
+    }
 
-    const valuesLength = props.valuesLength.map((item, i) => <Dropdown.Item key={i} href="#">{item}</Dropdown.Item>)
-    const loading = props.valuesLength === undefined ? 'loading' : null
-    const content = props.valuesLength !== undefined ? valuesLength : null
+    // const [title, setTitle] = useState('Длина')
+    // const [showList, setShow] = useState(false)
     
-    return (
-        <Col className={`${props.class}`}>
-            <InputGroup className='mb-3'>
-                <DropdownButton
-                    variant="outline-secondary"
-                    title="Длина"
-                    id="length-filter"
-                >
-                    {loading}
-                    {content}
-                </DropdownButton>
-                <FormControl onChange={(e) => props.updateLength(e.currentTarget.value, 'length')} aria-label="Text input with dropdown button" />
-            </InputGroup>
-        </Col>
-    )
+    setTitle(title) {
+        this.setState({ title: title })
+    }
+
+
+    render() {
+        const valuesLength = this.props.valuesLength.map((item, i) => <Dropdown.Item onClick={(e) => this.setTitle(item)} key={i} href="#">{item}</Dropdown.Item>)
+        const loading = this.props.valuesLength === undefined ? 'loading' : null
+        const content = this.props.valuesLength !== undefined ? valuesLength : null
+
+        return (
+            <Col className={`${this.props.class}`}>
+                <InputGroup className='mb-3'>
+                    <DropdownButton
+                        
+                        variant="outline-secondary"
+                        title={this.state.title}
+                        id="length-filter"
+                    >
+
+                        {loading}
+                        {content}
+                    </DropdownButton>
+                    <FormControl onChange={e => { this.props.updateLength(e.currentTarget.value, 'length') }} aria-label="Text input with dropdown button" />
+
+                </InputGroup>
+            </Col>
+        )
+    }
+
+
 }
 
 export default Length
