71d4625059427a62f3c9d326dbeffa8424114015
Nikita <norenkonikita@gmail.com>
2021-12-08 07:36:39 +0200

    done with length filter

--- src/Filter/Length.js
+++ src/Filter/Length.js
@@ -1,44 +1,65 @@
 
-import React, { Component, useState } from "react"
+import React, { Component } from "react"
 import { Col, InputGroup, DropdownButton, Dropdown, FormControl } from "react-bootstrap"
 
 class Length extends Component {
 
-    // const [title, setTitle] = useState('Длина')
-    // const [showList, setShow] = useState(false)
     constructor(props) {
         super(props)
         this.state = {
-            title: 'Длина'
-        };
-        // https://ru.reactjs.org/docs/refs-and-the-dom.html
+            title: 'Длина',
+            show: false,
+            value: ''
+        }
         this.dropdownRef = React.createRef();
+        this.dropdownButtonRef = React.createRef();
     }
     setTitle(title) {
         this.setState({ title: title })
     }
+    inputHandler(e) {
+        this.setState({value: e})
+        if (this.dropdownRef.current.value !== '') {
+            this.setState({show: true})
+        } else {
+            this.setState({show: false})
+        }
+    }
+    onDropDownButton(e) {
+        if (e.toLowerCase() === 'button') {
+            if (this.state.show === true) {
+                this.setState({show: false})    
+            } else {
+                this.setState({show: true})
+            }
+        } else {
+            this.setState({show: false})
+        }
+    }
+    
 
     render() {
         
         const valuesLength = this.props.valuesLength.map((item, i) => <Dropdown.Item onClick={(e) => this.setTitle(item)} key={i} href="#">{item}</Dropdown.Item>)
         const loading = this.props.valuesLength === undefined ? 'loading' : null
         const content = this.props.valuesLength !== undefined ? valuesLength : null
-        console.log(this.dropdownRef);
+        
         return (
             <Col className={`${this.props.class}`}>
-                <InputGroup className='mb-3'>
+                <InputGroup onClick={e => this.onDropDownButton(e.target.nodeName)}  className='mb-3'>
                     <DropdownButton
-                        ref={this.dropdownRef}
+                        show={this.state.show}
+                        className='test'
                         variant="outline-secondary"
-                        title={this.state.title}
+                        title={this.state.title === 'Длина' ? 'Длина' : this.state.title + ' мм'}
                         id="length-filter"
+                        
                     >
     
                         {loading}
                         {content}
                     </DropdownButton>
-                    <FormControl onChange={e => { this.props.updateLength(e.currentTarget.value, 'length') }} aria-label="Text input with dropdown button" />
-    
+                    <FormControl value={this.state.title === 'Длина' ?  this.state.value  : ''} ref={this.dropdownRef} onChange={e => { this.props.updateLength(e.currentTarget.value, 'length'); this.inputHandler(e.currentTarget.value) }} aria-label="Text input with dropdown button" />
                 </InputGroup>
             </Col>
         )
