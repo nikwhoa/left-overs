35c9767b04c18a145975cfcafa0ab4c5608f4ecb
Nikita <norenkonikita@gmail.com>
2021-12-09 06:31:02 +0200

    width filter

--- src/Filter/Width.js
+++ src/Filter/Width.js
@@ -1,29 +1,60 @@
-import { Component } from "react"
+import React, { Component } from "react"
 import { Col, InputGroup, DropdownButton, Dropdown, FormControl } from "react-bootstrap"
 
 
 
 class Width extends Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            title: 'Ширина',
+            show: false,
+            value: ''
+        }
+        this.dropdownRef = React.createRef();
+        this.dropdownButtonRef = React.createRef();
+    }
+    setTitle(title) {
+        this.setState({title: title})
+    }
+    inputHandler(e) {
+        this.setState({value: e})
+        if (this.dropdownRef.current.value !== '') {
+            this.setState({show: true})
+        } else {
+            this.setState({show: false})
+        }
+    }
+    onDropDownButton(e) {
+        if (e.toLowerCase() === 'button') {
+            this.setState({show: !this.state.show})
+        } else {
+            this.setState({show: false})
+        }
+    }
+
+
     render() {
         
-        const ValuesWidth = () => this.props.valuesWidth.map((item, i) => <Dropdown.Item key={i} href="#">{item}</Dropdown.Item>)
+        const ValuesWidth = () => this.props.valuesWidth.map((item, i) => <Dropdown.Item onClick={(e) => this.setTitle(item)} key={i} href="#">{item}</Dropdown.Item>)
             
         const loading = this.props.valuesWidth === undefined ? 'loading' : null
         const content = this.props.valuesWidth !== undefined ? <ValuesWidth /> : null
         
         return (
             <Col>
-                <InputGroup className="mb-3">
+                <InputGroup onClick={e => this.onDropDownButton(e.target.nodeName)} className="mb-3">
                     <DropdownButton
+                        show={this.state.show}
                         variant="outline-secondary"
-                        title="Ширина"
+                        title={this.state.title === 'Ширина' ? 'Ширина' : this.state.title + ' мм'}
                         id="width-filter"
                     >
                         {loading}
                         {content}
                     </DropdownButton>
-                    <FormControl onChange={(e) => this.props.updateWidth(e.currentTarget.value, 'width')} aria-label="Text input with dropdown button" />
+                    <FormControl type='number' value={this.state.title === 'Ширина' ? this.state.value : ''} ref={this.dropdownRef} onChange={(e) => { this.props.updateWidth(e.currentTarget.value, 'width'); this.inputHandler(e.currentTarget.value) }} aria-label="Text input with dropdown button" />
                 </InputGroup>
             </Col>
         )
