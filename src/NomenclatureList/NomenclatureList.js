079daa3ad9f77f770812b56ab87a90ceba42ba6e
Nikita <norenkonikita@gmail.com>
2022-01-08 14:49:33 +0200

    prepare to sort nomenclature list

--- src/NomenclatureList/NomenclatureList.js
+++ src/NomenclatureList/NomenclatureList.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useEffect } from 'react';
-import { Container, Row } from 'react-bootstrap';
+import { Button, Col, Container, Row } from 'react-bootstrap';
 
 
 const NomenclatureList = (props) => {
@@ -8,62 +8,63 @@ const NomenclatureList = (props) => {
 
   useEffect(() => {
     toShowNomenclatures()
-  })
+  }, [])
 
   const toShowNomenclatures = () => {
-    let visibleNomenclatures = this.props.nomenclatures.filter(item =>
-      item.length > this.props.data.length
-      && item.height > this.props.data.height
-      && item.width > this.props.data.width
-      && item.material === this.props.data.material)
+    let visibleNomenclatures = props.nomenclatures.filter(item =>
+      item.length > props.data.length
+      && item.height > props.data.height
+      && item.width > props.data.width
+      && item.material === props.data.material)
       .sort((a, b) => a.width - b.width)
-    
-      setNomenclatures({ visibleNomenclatures })
+
+    setNomenclatures(visibleNomenclatures)
   }
 
 
-    const loading = showNomenclatures === null ? 'loading' : null
-    const content = showNomenclatures !== null ? showNomenclatures.map(item =>
+  const loading = showNomenclatures === null ? 'loading' : null
+  const content = showNomenclatures !== null ? showNomenclatures.map((item, i) =>
 
-      <div className="col-12 col-sm-6 col-md-4 p-4">
-        <ul className='result-item'>
-          <li>
-            <p style={{ textAlign: 'center', fontWeight: 'bold', margin: '0 auto' }}>
-              {item.title}
-            </p>
-          </li>
-          <li>
-            <b>Номенклатура: </b> {item.code}
-          </li>
-          <li>
-            <b>Материал:</b> {item.material}
-          </li>
-          <li>
-            <b>Ширина:</b> {item.width} мм <br />
-          </li>
-          <li>
-            <b>Высота:</b> {item.height} мм<br />
-          </li>
-          <li>
-            <b>Длина:</b> {item.length} мм<br />
-          </li>
-          <li>
-            <b>Цена:</b> {item.price} &#8372;
-          </li>
-        </ul>
-      </div>
+    <div key={`${item.code}_${i}`} className="col-12 col-sm-6 col-md-4 p-4">
+      <ul className='result-item'>
+        <li>
+          <p style={{ textAlign: 'center', fontWeight: 'bold', margin: '0 auto' }}>
+            {item.title}
+          </p>
+        </li>
+        <li>
+          <b>Номенклатура: </b> {item.code}
+        </li>
+        <li>
+          <b>Материал:</b> {item.material}
+        </li>
+        <li>
+          <b>Ширина:</b> {item.width} мм <br />
+        </li>
+        <li>
+          <b>Высота:</b> {item.height} мм<br />
+        </li>
+        <li>
+          <b>Длина:</b> {item.length} мм<br />
+        </li>
+        <li>
+          <b>Цена:</b> {item.price} &#8372;
+        </li>
+      </ul>
+    </div>
 
-    ) : null
-
-    return (
-      <Container>
-        {loading}
-        <Row className='mt-5'>
-          {content === null ? 'ыыы' : content}
-        </Row>
-      </Container>
-    );
-  }
+  ) : null
+  const sort = <Row>{content === true ? null : <Col className='col-12'>Сортировать по: <Button className='sort-btn' variant='light'>Ширине</Button><Button className='sort-btn' variant='light'>Длине</Button></Col>}</Row>
+  return (
+    <Container>
+      {loading}
+      <Row className='mt-5'>
+      {sort}
+        {content === null ? 'ыыы' : content}
+      </Row>
+    </Container>
+  );
+}
 
 
 export default NomenclatureList;
