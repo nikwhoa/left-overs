f90d638b5a986d6238299011caa05937d33e7cb8
Nikita <norenkonikita@gmail.com>
2022-01-08 13:50:33 +0200

    before functionial component

--- src/NomenclatureList/NomenclatureList.js
+++ src/NomenclatureList/NomenclatureList.js
@@ -13,7 +13,12 @@ class NomenclatureList extends Component {
   }
 
   toShowNomenclatures = () => {
-    let visibleNomenclatures = this.props.nomenclatures.filter(item => item.length > this.props.data.length && item.height > this.props.data.height && item.width > this.props.data.width && item.material === this.props.data.material).sort((a, b) => a.code - b.code)
+    let visibleNomenclatures = this.props.nomenclatures.filter(item =>
+      item.length > this.props.data.length
+      && item.height > this.props.data.height
+      && item.width > this.props.data.width
+      && item.material === this.props.data.material)
+      .sort((a, b) => a.width - b.width)
     this.setState({ showNomenclatures: visibleNomenclatures })
   }
 
@@ -21,38 +26,35 @@ class NomenclatureList extends Component {
     const loading = this.state.showNomenclatures === null ? 'loading' : null
     const content = this.state.showNomenclatures !== null ? this.state.showNomenclatures.map(item =>
 
-      <div className="col-12 col-sm-6 col-md-4 p-5">
-        <ul>
+      <div className="col-12 col-sm-6 col-md-4 p-4">
+        <ul className='result-item'>
           <li>
-            <b>Номенклатура: </b> {item.code}
-          </li>
-          <li>
-            <b>Название: </b>
-            <p style={{textAlign: 'right'}}>
+            <p style={{ textAlign: 'center', fontWeight: 'bold', margin: '0 auto' }}>
               {item.title}
             </p>
+          </li>
+          <li>
+            <b>Номенклатура: </b> {item.code}
           </li>
           <li>
             <b>Материал:</b> {item.material}
           </li>
           <li>
-            <b>Ширина:</b> {item.width} <br />
+            <b>Ширина:</b> {item.width} мм <br />
           </li>
           <li>
-            <b>Высота:</b> {item.height} <br />
+            <b>Высота:</b> {item.height} мм<br />
           </li>
           <li>
-            <b>Длина:</b> {item.length} <br />
+            <b>Длина:</b> {item.length} мм<br />
           </li>
           <li>
-            <b>Цена:</b> {item.price}
+            <b>Цена:</b> {item.price} &#8372;
           </li>
         </ul>
       </div>
 
     ) : null
-
-    console.log(content)
 
     // const nomenclatures = this.state.showNomenclatures.map(item => <Row>{item.code}</Row>)
     return (
